7b40c9fdd07c22a4c943de208add9d6a29d3672a
Martin Liu <liumartin@google.com>
2025-10-29 18:35:24 +0545

    rubyx: Disable memcg
    Pixel kernel don't use MEMCG, let's disable it to minimize the overhead.
    
    Signed-off-by: Aeron-Aeron <aeronrules2@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,6 +61,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_RAMDISK_USE_LZ4 := true
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.tee_type=2
+BOARD_KERNEL_CMDLINE += cgroup_disable=memory
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 BOARD_MKBOOTIMG_ARGS := --base $(BOARD_KERNEL_BASE)
